806516c08c096b94b0027e3bc6afc175284943fa
DHerrera85 <74433631+DHerrera85@users.noreply.github.com>
2021-05-04 19:27:59 -0300

    Add files via upload

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace TurArg
+{
+    public class BundleConfig
+    {
+        // Para obtener más información sobre las uniones, visite
+        // https://go.microsoft.com/fwlink/?LinkId=301862
+
+       public static void RegisterBundles(BundleCollection bundles)
+            {
+                bundles.Add(new Bundle("~/Script/Bundles").Include(
+                "~/Bundles/inline.*",
+                "~/Bundles/polyfills.*",
+                "~/Bundles/scripts.*",
+                "~/Bundles/runtime.*",
+                "~/Bundles/vendor.*",
+                "~/Bundles/zone.*",
+                "~/Bundles/main.*"));
+                bundles.Add(new ScriptBundle("~/Content/Styles").Include(
+                    "~/bundles/styles.*"));
+            
+        }
+    }
+}
